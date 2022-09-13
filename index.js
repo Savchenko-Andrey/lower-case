970b381df1a22d1147c9c373829b1f5a7fce7061
Savchenko-Andrey <dracxm95@gmail.com>
2022-09-13 14:45:24 +0300

    change 2

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const textInput = document.querySelector('.texteria__input');
 const output = document.querySelector('.texteria__output');
+const btn = document.querySelector('.btn');
 
 textInput.addEventListener("input", (event) => {
     qwe  = event.currentTarget.value;
     output.textContent = qwe.toLowerCase()
   });
+
+
+  function copy(event) {
+    event.preventDefault();
+    const copyText = document.querySelector('#output');
+    copyText.select()
+    document.execCommand('copy')
+  }
+
+  document.querySelector('#copy').addEventListener('click', copy)
